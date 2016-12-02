c02a59a5b42fb7224a25e0ea7bb95351360245d0
outdooras <markus@outdoor.no>
2016-12-02 12:11:57 +0100

    #

---
+++ instafeed/instafeed.js
@@ -0,0 +1,21 @@
+var feed = new Instafeed({
+        get: 'user',
+        userId: '2162912851',
+        accessToken: '2162912851.1677ed0.eb4c604e9f6b42d788f586a965d7b7af',
+  limit: 6,
+		sortBy: 'most-recent',
+  after: function () {
+    var images = $("#instafeed").find('a');
+    $.each(images, function(index, image) {
+      var delay = (index * 75) + 'ms';
+      $(image).css('-webkit-animation-delay', delay);
+      $(image).css('-moz-animation-delay', delay);
+      $(image).css('-ms-animation-delay', delay);
+      $(image).css('-o-animation-delay', delay);
+      $(image).css('animation-delay', delay);
+      $(image).addClass('animated flipInX');
+    });
+  },
+  template: '<div class="col-lg-2 col-sm-12 col-xs-12"><a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a></div>'
+});
+feed.run();
