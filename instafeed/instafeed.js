1b78b019727fd933436470cd8b54d8afce63ad8f
outdooras <markus@outdoor.no>
2016-12-02 12:50:24 +0100

    #

--- instafeed/instafeed.js
+++ instafeed/instafeed.js
@@ -2,20 +2,20 @@ var feed = new Instafeed({
         get: 'user',
         userId: '2162912851',
         accessToken: '2162912851.1677ed0.eb4c604e9f6b42d788f586a965d7b7af',
-  limit: 6,
+		limit: 6,
 		sortBy: 'most-recent',
-  after: function () {
-    var images = $("#instafeed").find('a');
-    $.each(images, function(index, image) {
-      var delay = (index * 75) + 'ms';
-      $(image).css('-webkit-animation-delay', delay);
-      $(image).css('-moz-animation-delay', delay);
-      $(image).css('-ms-animation-delay', delay);
-      $(image).css('-o-animation-delay', delay);
-      $(image).css('animation-delay', delay);
-      $(image).addClass('animated flipInX');
-    });
-  },
-  template: '<div class="col-lg-2 col-sm-12 col-xs-12"><a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a></div>'
+		  after: function () {
+			var images = $("#instafeed").find('a');
+			$.each(images, function(index, image) {
+			  var delay = (index * 75) + 'ms';
+			  $(image).css('-webkit-animation-delay', delay);
+			  $(image).css('-moz-animation-delay', delay);
+			  $(image).css('-ms-animation-delay', delay);
+			  $(image).css('-o-animation-delay', delay);
+			  $(image).css('animation-delay', delay);
+			  $(image).addClass('animated flipInX');
+			});
+		  },
+		template: '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 clearfix listItem"><a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a></div>'
 });
 feed.run();
