e2989322038668d35c8b810a2b89cdc288f884e5
outdooras <markus@outdoor.no>
2016-12-05 11:03:06 +0100

    #

--- instafeed/instafeed.js
+++ instafeed/instafeed.js
@@ -4,7 +4,7 @@ jQuery( document ).ready(function($) {
  userId: 2162912851,
  accessToken: '2162912851.1677ed0.eb4c604e9f6b42d788f586a965d7b7af',
  resolution: 'thumbnail',
- template: '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12"><a href="{{link}}" target="_blank"><img src="{{image}}" style="border-radius: 25px"; /><div class="likes">&hearts; {{likes}}</div></a></div>',
+ template: '<a href="{{link}}" target="_blank"><img src="{{image}}" style="border-radius: 25px"; /><div class="likes">&hearts; {{likes}}</div></a>',
  target: 'instafeed',
  
  after: function() {
