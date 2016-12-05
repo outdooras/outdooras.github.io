2d7475a15e212e67a4aa3456851d143e0f115bbc
outdooras <markus@outdoor.no>
2016-12-05 16:15:16 +0100

    #

--- instafeed/instafeed.js
+++ instafeed/instafeed.js
@@ -10,11 +10,12 @@ jQuery( document ).ready(function($) {
  after: function() {
  $('.owl-carousel').owlCarousel({
 	items : 1, //1 items above 1000px browser width
+	/*
 	itemsDesktop : [1000,1], //1 items between 1000px and 901px
 	itemsDesktopSmall : [900,1], // 1 items between 900px and 601px
 	itemsTablet: [600,1], //1 items between 600 and 0
 	itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-
+	*/
 	margin:0.5,
 	
 	//Pagination
